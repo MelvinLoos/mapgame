4ca30a36a32f69bb6db85bd13d64916222795b1e
Melvin <melvin@melvinloos.nl>
2020-05-06 02:00:19 +0200

    Add 3d layer to map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -6,21 +6,19 @@
     :maxZoom="maxZoom"
     :center="coordinates"
     :zoom="18"
+    @load="onMapLoaded"
   >
-    <MglMarker :coordinates="coordinates">    
-        <b-icon
-            icon="user-circle"
-            size="is-medium"
-            slot="marker"
-            v-bind:style="{color: convertIdToColor(userId)}">
-            User
-        </b-icon>
+    <MglMarker :coordinates="coordinates">
+      <b-icon
+        icon="user-circle"
+        size="is-medium"
+        slot="marker"
+        v-bind:style="{color: convertIdToColor(userId)}"
+      >User</b-icon>
       <MglPopup>
         <div class="card">
           <header class="card-header">
-            <p class="card-header-title">
-              This is you!
-            </p>
+            <p class="card-header-title">This is you!</p>
           </header>
           <div class="card-content">
             <div class="content">
@@ -32,26 +30,23 @@
         </div>
       </MglPopup>
     </MglMarker>
-    <MglMarker 
+    <MglMarker
       v-for="(user, index) in users"
-      v-bind:item="user" 
-      v-bind:index="index" 
-      v-bind:key="user.id" 
+      v-bind:item="user"
+      v-bind:index="index"
+      v-bind:key="user.id"
       v-bind:coordinates="[user.latitude, user.longitude]"
-    >    
+    >
       <b-icon
-          icon="user-circle"
-          size="is-medium"
-          slot="marker"
-          v-bind:style="{color: convertIdToColor(index)}">
-          User
-      </b-icon>
+        icon="user-circle"
+        size="is-medium"
+        slot="marker"
+        v-bind:style="{color: convertIdToColor(index)}"
+      >User</b-icon>
       <MglPopup>
         <div class="card">
           <header class="card-header">
-            <p class="card-header-title">
-              {{ user.name }}
-            </p>
+            <p class="card-header-title">{{ user.name }}</p>
           </header>
           <div class="card-content">
             <div class="content">
@@ -67,40 +62,91 @@
 </template>
 
 <script>
-import Mapbox from 'mapbox-gl';
-import { MglMap, MglMarker, MglPopup } from 'vue-mapbox';
-import { hashCode, intToRGB } from '../utils';
+import Mapbox from "mapbox-gl";
+import { MglMap, MglMarker, MglPopup } from "vue-mapbox";
+import { hashCode, intToRGB } from "../utils";
 
 export default {
-  name: 'Map',
+  name: "Map",
   components: {
     MglMap,
     MglMarker,
     MglPopup
   },
   props: {
-      coordinates: Array,
-      userId: String,
-      users: Object
+    coordinates: Array,
+    userId: String,
+    users: Object
   },
   data() {
     return {
       accessToken: process.env.VUE_APP_MAPBOX_TOKEN,
-      mapStyle: 'mapbox://styles/mapbox/dark-v10',
+      mapStyle: "mapbox://styles/mapbox/dark-v10",
       minZoom: 14,
       maxZoom: 22
     };
   },
   methods: {
     convertIdToColor(id) {
-      if (!id) return '#FFFFFF';
-      return '#' + intToRGB(hashCode(id));
+      if (!id) return "#FFFFFF";
+      return "#" + intToRGB(hashCode(id));
+    },
+    onMapLoaded(event) {
+      // in component
+      this.map = event.map;
+      // Insert the layer beneath any symbol layer.
+      var layers = this.map.getStyle().layers;
+
+      var labelLayerId;
+      for (var i = 0; i < layers.length; i++) {
+        if (layers[i].type === "symbol" && layers[i].layout["text-field"]) {
+          labelLayerId = layers[i].id;
+          break;
+        }
+      }
+
+      this.map.addLayer(
+        {
+          id: "3d-buildings",
+          source: "composite",
+          "source-layer": "building",
+          filter: ["==", "extrude", "true"],
+          type: "fill-extrusion",
+          minzoom: 15,
+          paint: {
+            "fill-extrusion-color": "#aaa",
+
+            // use an 'interpolate' expression to add a smooth transition effect to the
+            // buildings as the user zooms in
+            "fill-extrusion-height": [
+              "interpolate",
+              ["linear"],
+              ["zoom"],
+              15,
+              0,
+              15.05,
+              ["get", "height"]
+            ],
+            "fill-extrusion-base": [
+              "interpolate",
+              ["linear"],
+              ["zoom"],
+              15,
+              0,
+              15.05,
+              ["get", "min_height"]
+            ],
+            "fill-extrusion-opacity": 0.6
+          }
+        },
+        labelLayerId
+      );
     }
   },
   created() {
     // We need to set mapbox-gl library here in order to use it in template
     this.mapbox = Mapbox;
-  },
+  }
 };
 </script>
 
